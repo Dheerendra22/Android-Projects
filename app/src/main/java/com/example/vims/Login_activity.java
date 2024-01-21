802b80279fb43f3d7ee97a2ae63ebe0a58625dcd
Dheerendra22 <dheerendramishra64646@gmail.com>
2024-01-22 02:26:12 +0530

    firbase start

--- app/src/main/java/com/example/vims/Login_activity.java
+++ app/src/main/java/com/example/vims/Login_activity.java
@@ -1,14 +1,26 @@
 package com.example.vims;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Login_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        Button login = findViewById(R.id.btnLogin);
+        login.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),Register_Activity.class)));
+
     }
 }
--- app/src/main/java/com/example/vims/Register_Activity.java
+++ app/src/main/java/com/example/vims/Register_Activity.java
@@ -1,14 +1,106 @@
 package com.example.vims;
 
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register_Activity extends AppCompatActivity {
+    EditText fullName,email,password,con_password,phone ;
+    Button register ;
+    TextView goToSign ;
+    ProgressBar pg ;
 
+   FirebaseAuth faut ;
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+
         setContentView(R.layout.activity_register);
+
+
+       //  find Id
+
+        fullName = findViewById(R.id.fullName);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        con_password = findViewById(R.id.ConPassword);
+        phone = findViewById(R.id.phoneNumber);
+        register = findViewById(R.id.btnRegister);
+        goToSign =findViewById(R.id.gotoSign_in);
+        pg = findViewById(R.id.progressbar);
+
+
+       //  Get instance
+
+        faut = FirebaseAuth.getInstance();
+
+
+
+
+
+register.setOnClickListener(v -> {
+
+    String name = fullName.getText().toString() ;
+    String mEmail = email.getText().toString().trim();
+    String mPassword = password.getText().toString().trim();
+    String conPassword = con_password.getText().toString().trim();
+    String mPhone = phone.getText().toString().trim();
+
+
+    if(TextUtils.isEmpty(name)){
+        fullName.setError("Full name is required!");
+        return;
     }
-}
+    if (TextUtils.isEmpty(mEmail)) {
+        email.setError("Email is required!");
+        return;
+    }
+    if (TextUtils.isEmpty(mPassword)) {
+        password.setError("Please enter Password");
+        return;
+    }
+    if (mPassword.length() < 6) {
+        password.setError("Password must be 6 letters or more! ");
+        return;
+    }
+    if (!mPassword.equals(conPassword)) {
+        con_password.setError("Enter password Correctly!");
+
+    }
+    if (mPhone.length() < 10) {
+        phone.setError("Enter correct phone number!");
+    }
+
+
+});
+
+
+
+
+    }
+
+
+
+
+
+    }
